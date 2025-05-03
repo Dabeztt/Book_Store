fba9acfa4acca44589b4544acc77d4bfc9042aa5
Dabeztt <phatdat1495@gmail.com>
2025-05-03 22:24:20 +0700

    Update home page and add router

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const links = [
     {
       title: "Trang chủ",
       link: "/",
-    },
-    {
-      title: "Thông tin liên hệ",
-      link: "/thong-tin-lien-he",
     },
     {
       title: "Sách",
@@ -25,33 +22,40 @@ const Navbar = () => {
   ];
   return (
     <div className="flex bg-zinc-800 text-white px-8 py-4 items-center justify-between">
-      <div className="flex items-center">
+      <Link to="/" className="flex items-center">
         <img
           className="h-10 me-4 filter invert"
           src="https://cdn-icons-png.flaticon.com/128/864/864685.png"
           alt="logo"
         />
         <h1 className="text-2xl font-semibold">BookStore</h1>
-      </div>
+      </Link>
       <div className="nav-links-bookstore flex items-center gap-4">
         <div className="flex gap-4">
           {links.map((items, i) => (
-            <div
+            <Link
+              to={items.link}
               className="hover:text-blue-500 transition-all duration-300"
               key={i}
             >
               {items.title}
               {""}
-            </div>
+            </Link>
           ))}
         </div>
         <div className="flex gap-4 flex">
-          <button className="px-2 py-1 border border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300">
+          <Link
+            to="/dang-nhap"
+            className="px-4 py-1 border border-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300"
+          >
             Đăng nhập
-          </button>
-          <button className="px-2 py-1 bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300">
+          </Link>
+          <Link
+            to="/dang-ky"
+            className="px-4 py-1 bg-blue-500 rounded hover:bg-white hover:text-zinc-800 transition-all duration-300"
+          >
             Đăng ký
-          </button>
+          </Link>
         </div>
       </div>
     </div>
