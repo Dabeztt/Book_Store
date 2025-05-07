a3e518982614d7bfb55f03506eccff7a241c079f
Dabeztt <phatdat1495@gmail.com>
2025-05-07 16:34:00 +0700

    Redux

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import AllBooks from "./pages/AllBooks";
 import LogIn from "./pages/Login";
 import SignUp from "./pages/SignUp";
@@ -13,19 +13,17 @@ import ViewBookDetails from "./components/ViewBookDetails/ViewBookDetails";
 const App = () => {
   return (
     <div>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/sach" element={<AllBooks />} />
-          <Route path="/gio-hang" element={<Cart />} />
-          <Route path="/thong-tin-ca-nhan" element={<Profile />} />
-          <Route path="/dang-nhap" element={<LogIn />} />
-          <Route path="/dang-ky" element={<SignUp />} />
-          <Route path="/chi-tiet-sach/:id" element={<ViewBookDetails />} />
-        </Routes>
-        <Footer />
-      </Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/sach" element={<AllBooks />} />
+        <Route path="/gio-hang" element={<Cart />} />
+        <Route path="/thong-tin-ca-nhan" element={<Profile />} />
+        <Route path="/dang-nhap" element={<LogIn />} />
+        <Route path="/dang-ky" element={<SignUp />} />
+        <Route path="/chi-tiet-sach/:id" element={<ViewBookDetails />} />
+      </Routes>
+      <Footer />
     </div>
   );
 };
